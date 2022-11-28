e6b987d4a703e455bc27d7cf2f3c0460b222f53a
Augusto Cezar Ribeiro Santana <augusto.santana@al.infnet.edu.br>
2022-11-28 11:00:00 -0300

    Sonarlint

--- src/main/java/br/com/infnet/persistencia/model/Categoria.java
+++ src/main/java/br/com/infnet/persistencia/model/Categoria.java
@@ -1,7 +1,18 @@
 package br.com.infnet.persistencia.model;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.util.List;
 
